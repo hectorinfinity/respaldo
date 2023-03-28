01adce20a4b112b75af4412d92d6953bbddf4821
st4l1nR <stalinramosbw@gmail.com>
2023-03-28 10:33:22 -0500

    Icon component fixed && CardEventOrder added

--- src/components/main/checkout/CardEventOrder.stories.tsx
+++ src/components/main/checkout/CardEventOrder.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import CardEventOrder, { props } from './CardEventOrder';
-import {faker} from '@faker-js/faker'
+import { faker } from '@faker-js/faker';
 
 export default {
   title: 'Molecules/CardEventOrder',
@@ -15,14 +15,18 @@ Default.args = {
   className: faker.random.word(),
   image: faker.image.image(),
   name: faker.random.words(3),
-  duration: faker.datatype.number(),
-  date: faker.date.future(),
-  countTickets: faker.datatype.number(),
+  time: faker.date.future(),
+  date: faker.datatype.number(),
   location: faker.address.city(),
   author: faker.name.findName(),
   spots: Array.from({ length: 5 }, () => ({
     row: faker.random.alphaNumeric(2),
     seat: faker.datatype.number(20),
-    section: faker.datatype.number(10)
-  }))
+    section: faker.datatype.number(10),
+  })),
+  countTickets: faker.datatype.number({ min: 1, max: 1000 }),
+  user: {
+    name: faker.name.fullName(),
+    lastFour: faker.datatype.number({ min: 111, max: 9999 }),
+  },
 };
--- src/components/main/checkout/CardEventOrder.tsx
+++ src/components/main/checkout/CardEventOrder.tsx
@@ -2,62 +2,117 @@ import React from 'react';
 import { classNames } from '@/helpers';
 import Image from 'next/image';
 import { Icon } from '@/components/commons';
-import formatDate from 'date-fns';
-import { useTranslations } from "next-intl";
+import { format } from 'date-fns';
+import { useTranslations } from 'next-intl';
 
 export type props = {
   className?: string;
   image: string;
   name: string;
-  duration: number;
   date: Date;
   countTickets: number;
   location: string;
-  author: string;
   spots: { row: string; seat: number; section: number }[];
+  user: {
+    name: string;
+    lastFour: number;
+  };
 };
 
 const CardEventOrder: React.FC<props> = ({
   className,
   image,
   name,
-  duration,
   date,
   countTickets,
   location,
-  author,
   spots,
+  user,
 }) => {
+  const t = useTranslations('Card_Event_Order');
   return (
-    <div className={classNames('rounded-2xl shadow-xl', className)}>
-      <div className="w-full h-12 bg-customPink"></div>
-      <div className="flex">
-        <div className="basis-1/3">
-          <Image src={image} width={300} height={300} alt={name}></Image>
+    <div
+      className={classNames(
+        'rounded-2xl shadow-xl overflow-hidden text-sm',
+        className
+      )}
+    >
+      <div className="w-full h-8 bg-customPink"></div>
+      <div className="flex ">
+        <div className="basis-1/3 ">
+          <div className="w-full h-full relative">
+            <Image className="object-cover" src={image} fill alt={name}></Image>
+          </div>
         </div>
-        <div className="grid grid-cols-2 gap-10 p-5 basis-2/3">
+        <div className="grid grid-cols-2 gap-10 py-4 px-5 sm:px-8 basis-2/3  ">
           <div>
             <p className="text-xl font-bold">{name}</p>
-            <div className="grid grid-cols-2 gap-5">
+            <div className="flex flex-col  gap-5 mt-5">
               <div className="flex items-center gap-3">
-                <Icon name="clock-outline"></Icon>
-                <p className="font-bold"></p>
+                <Icon className="w-5 h-5" name="calendar-outline" />
+                <p className="font-bold">
+                  {t('date')}:{' '}
+                  <span className="font-medium">
+                    {format(date, 'hh:mm')} hrs
+                  </span>
+                </p>
               </div>
-              <p></p>
+
               <div className="flex items-center gap-3">
-                <Icon name="calendar-outline"></Icon>
-                <p className="font-bold"></p>
+                <Icon className="w-5 h-5" name="clock-outline" />
+                <p className="font-bold">
+                  {t('time')}:{' '}
+                  <span className="font-medium">
+                    {format(date, 'MMM yyyy')}
+                  </span>
+                </p>
               </div>
-              <p></p>
+
               <div className="flex items-center gap-3">
-                <Icon name="person-outline"></Icon>
-                <p className="font-bold"></p>
+                <Icon className="w-5 h-5" name="person-outline" />
+                <p className="font-bold">
+                  {t('tickets')}:{' '}
+                  <span className="font-medium">{countTickets}</span>
+                </p>
               </div>
               <div className="flex items-center gap-3">
-                <Icon name="map-pin"></Icon>
-                <p className="font-bold"></p>
+                <Icon className="w-5 h-5" name="map-pin" />
+                <p className="font-bold">
+                  {t('tickets')}:{' '}
+                  <span className="font-medium">{location}</span>
+                </p>
               </div>
-              <p></p>
+            </div>
+          </div>
+          <div>
+            <p className="p font-bold">{t('name')}</p>
+            <p className="mt-2">{user.name}</p>
+            <div className="mt-8">
+              <table>
+                <thead>
+                  <th className="pr-5">Row</th>
+                  <th className="pr-5">Seat</th>
+                  <th>Section</th>
+                </thead>
+                <tbody>
+                  {spots.map(({ row, seat, section }, idx) => (
+                    <tr key={idx}>
+                      <td className="pr-5">{row}</td>
+                      <td className="pr-5">{seat}</td>
+                      <td className="pr-5">{section}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <div className="mt-5">
+                <p className="font-extrabold">{t('payment_method')}</p>
+                <p className="mt-2">
+                  <p className="flex gap-3 items-center">
+                    <Icon name="master-card"></Icon>
+                    <p>XXXX XXXX XXXX {user.lastFour}</p>
+                  </p>
+                </p>
+              </div>
             </div>
           </div>
         </div>
