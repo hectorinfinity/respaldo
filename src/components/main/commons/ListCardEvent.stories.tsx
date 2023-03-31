aa8daf3b78faf12613880a94e51702002d2aa11a
st4l1nR <stalinramosbw@gmail.com>
2023-03-31 15:11:12 -0500

    finish ListCardEvent component

--- src/components/main/commons/ListCardEvent.stories.tsx
+++ src/components/main/commons/ListCardEvent.stories.tsx
@@ -31,6 +31,7 @@ const items = Array.from({ length: 10 }, () => ({
 
 export const Default = Template.bind({});
 Default.args = {
+  title: faker.lorem.word(),
   layout: 'grid',
   items,
   loading: false,
@@ -39,19 +40,49 @@ Default.args = {
 
 export const LayoutColumn = Template.bind({});
 LayoutColumn.args = {
+  title: faker.lorem.word(),
   layout: 'column',
   items,
   loading: false,
   totalDocs: 10,
 };
 
-
 export const LayoutSwiper = Template.bind({});
 LayoutSwiper.args = {
+  title: faker.lorem.word(),
   layout: 'swiper',
   items,
   loading: false,
   totalDocs: 10,
 };
 
+export const WithControls = Template.bind({});
+WithControls.args = {
+  title: faker.lorem.word(),
+  layout:"grid",
+  items,
+  loading: false,
+  totalDocs: 10,
+  controls: true,
+};
 
+export const Loading = Template.bind({});
+Loading.args = {
+  title: faker.lorem.word(),
+  layout:"grid",
+  items,
+  loading: true,
+  totalDocs: 10,
+  controls: true,
+};
+
+
+export const NotFound = Template.bind({});
+NotFound.args = {
+  title: faker.lorem.word(),
+  layout:"grid",
+  items,
+  loading: false,
+  totalDocs: 0,
+  controls: true,
+};
