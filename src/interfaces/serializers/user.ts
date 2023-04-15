ee0a7ae7f6011bde6b2bbd195a0c774a41f9d9bb
Hector Ruiz <hectorruizlaph@gmail.com>
2023-04-14 21:50:55 -0600

    setting & delete done

--- src/interfaces/serializers/user.ts
+++ src/interfaces/serializers/user.ts
@@ -1,46 +1,46 @@
 export interface UserPayment {
-    stripe?: string | null
-    paypal?: string | null
+  stripe?: string | null;
+  paypal?: string | null;
 }
 
 export interface UserCard {
-    stripeId: string
-    card: string
-    type: string
-    name: string
-    exp: string
+  stripeId: string;
+  card: string;
+  type: string;
+  name: string;
+  exp: string;
 }
 
 export interface UserPromoter {
-    selfie: string
-    id_front: string
-    id_back: string
-    status: number
+  selfie: string;
+  id_front: string;
+  id_back: string;
+  status: number;
 }
 
 interface UserStat {
-    impressions: number
-    clicks: number
+  impressions: number;
+  clicks: number;
 }
 
 export interface UserPublicityStat {
-    app: UserStat
-    web: UserStat
+  app: UserStat;
+  web: UserStat;
 }
 
 export interface UserSettings {
-    lang: string
-    radius_search: string
-    receive_mail: boolean
-    receive_notifications: boolean
+  lang: string;
+  radius_search?: string;
+  receive_mail: boolean;
+  receive_notifications: boolean;
 }
 
 export interface UserTicketExchange {
-    user_id: string
-    type: boolean
+  user_id: string;
+  type: boolean;
 }
 
 export interface UserTicketSettings {
-    resale: boolean
-    transfer: boolean
+  resale: boolean;
+  transfer: boolean;
 }
