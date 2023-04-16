80967149f122f110212afb8d3f8a63e6dd1a880c
Hector Ruiz <hectorruizlaph@gmail.com>
2023-04-16 14:10:18 -0600

    follows done pending endpoints

--- src/components/admin/profile/customCategory.tsx
+++ src/components/admin/profile/customCategory.tsx
@@ -1,40 +1,47 @@
-import Image from "next/image";
+import Image from 'next/image';
 // Icons
 import { CheckIcon } from '@heroicons/react/20/solid';
+// Interfaces
+import { EventCategory } from '@/interfaces/event';
 
-type Props = {
-    name: string,
-    srcImage: string,
-    selected?: boolean
-}
-
-export const CustomCategory = ({ name, srcImage, selected }: Props) => {
-    
-    return (
-        <label>
-            <div className="relative ">
-                <input
-                    id={srcImage}
-                    type="checkbox"
-                    className="customCheckboxes peer absolute opacity-0 "
-                />
-                <Image
-                    width={120}
-                    height={120}
-                    src={srcImage}
-                    className={`object-contain ${selected ? '' : 'grayscale peer-checked:grayscale-0'}`}
-                    alt={name}
-                />
-                <div className={`tick absolute top-0 right-0 flex h-5 w-5 items-center justify-center rounded-full bg-customGreen peer-checked:opacity-100 ${selected ? '' : 'opacity-0'}`}>
-                    <CheckIcon
-                        name="check"
-                        className="iconshow absolute text-sm text-white"
-                    />
-                </div>
-            </div>
-            <p className="text-sm text-customGray text-center py-5">
-                {name}
-            </p>
-        </label>
-    )
-}
+export const CustomCategory = ({
+  _id,
+  category,
+  picture,
+  color,
+  status,
+}: EventCategory) => {
+  return (
+    <label>
+      <div className="relative ">
+        <input
+          //   id={_id}
+          type="checkbox"
+          className="customCheckboxes peer absolute opacity-0 "
+        />
+        <Image
+          width={120}
+          height={120}
+          src={picture}
+          className={`object-contain ${
+            status ? '' : 'grayscale peer-checked:grayscale-0'
+          }`}
+          alt={category?.[0].name}
+        />
+        <div
+          className={`tick absolute top-0 right-0 flex h-5 w-5 items-center justify-center rounded-full bg-customGreen peer-checked:opacity-100 ${
+            status ? '' : 'opacity-0'
+          }`}
+        >
+          <CheckIcon
+            name="check"
+            className="iconshow absolute text-sm text-white"
+          />
+        </div>
+      </div>
+      <p className="text-sm text-customGray text-center py-5">
+        {category?.[0].name}
+      </p>
+    </label>
+  );
+};
